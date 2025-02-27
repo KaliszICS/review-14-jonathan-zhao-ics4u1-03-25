8ef7389d9a2c9d164214006fc577c71c7d7c5197
jonathan-zhao-ics4u1-03-25 <jonathan.zhao@student.tdsb.on.ca>
2025-02-27 23:02:17 +0000

    Done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,6 +1,68 @@
+
 public class PracticeProblem {
+	
 	public static void main(String args[]) {
 
 	}
+	public static int calculate(int num1, int num2, char c) {
+	int sum = 0;
+		switch (c) {
+		case '+': 
+			sum = (num1 + num2);
+			break;
+		case '-': 
+			sum = (num1 - num2);
+			break;
+		case '*': 
+			sum = (num1 * num2);
+			break;
+		case '/': 
+			sum = (int)Math.floor(num1 / num2);
+			break;
+		case '%': 
+			sum = (num1 % num2);
+			break;
+		case '^': 
+			sum = (int)(Math.pow(num1, num2));
+			break;
+		}
+		return sum;
+	}
 
+	final static int MAXIMUM = 10;
+	public static boolean totalWordsChecker(String word) {
+			return word.split("[^A-Za-z'_]+").length <= MAXIMUM;
+	
+
+		
+	}
+
+		
+	
+
+	
+	public static String minString(String s1, String s2, String s3){
+		s1 = s1.toLowerCase();
+		s2 = s2.toLowerCase();
+		s3 = s3.toLowerCase();
+		if (s1.compareTo(s2) < 0){
+			if (s1.compareTo(s3) < 0){
+				return s1;
+			} else {
+				return s3;
+			}
+
+		} else {
+			if (s2.compareTo(s3) < 0){
+				return s2;
+			} else {
+				return s3;
+		}
+	
+	
+	}
+	
+	}
 }
+
+
